fd66ca882e37ad2cc885fb70a933a31c0996aef1
cilenedv <cilenedelacruz@gmail.com>
2016-10-19 23:04:40 -0500

    subiendo localizacion y menu

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 	$(".num_1").focus();
 	$("#numero").focus();
+	$("#name").focus();
+
 	$("#numero").keydown(function(evento) {
 		var ascii = evento.keyCode;
 		if (ascii == 8 || (ascii >= 48 && ascii <= 57)) {
@@ -9,6 +11,7 @@ $(document).ready(function() {
 			return false;
 		}
 	});
+
 	$("#numero").keyup(function(evento) {
 		var numeroCelular=$(this).val();
 		var longitud = numeroCelular.length;
@@ -20,6 +23,7 @@ $(document).ready(function() {
 			$("#next").removeAttr("href");
 		}
 	});
+
 	$("#next").click(function(evento){
 		var longitud=$("#numero").val().length;
 		var codigo=Math.round(Math.random()*890+100);
@@ -30,6 +34,16 @@ $(document).ready(function() {
      		alert(mensaje);
 		}	
 	});
+
+	$("#resend").click(function(evento){
+		var codigo=Math.round(Math.random()*890+100);
+		localStorage.setItem("clave",codigo);
+		var mensaje="Lab - "+codigo;
+     	alert(mensaje);
+     	$(".num_1").focus();
+
+	});
+
 	var numeroValido=localStorage.getItem("numeroCelular");
 	$("#cop_num").append(numeroValido);
 
@@ -45,9 +59,32 @@ $(document).ready(function() {
 		}
 		if(numeroClave!=numeroRandon && num_1!=0){
 			alert("Su clave es incorrecto");
+			$(".num_clave").val("");
+			$(".num_1").focus();
 		}
-
 	});
+
+	$(".num_clave").keyup(function(evento){
+		var ascii = evento.keyCode;
+		var longitud = $(this).val().length;
+		if (longitud == 1) {
+	        $(this).next().focus();
+	    }
+	    if (ascii == 8) {
+	    	$(this).prev().focus();
+	    }
+	});
+
+	$(".num_clave").keydown(function(){
+		var ascii = evento.keyCode;
+		var longitud = $(this).val().length;
+		if (ascii == 8 || (ascii >= 48 && ascii <= 57 && longitud == 0)) {
+			return true;
+		} else {
+			return false;
+		}
+	});
+
 	$("#next3").click(function(evento){
 		if($("#check").is(":checked")){
 			if(validar()){
@@ -55,25 +92,29 @@ $(document).ready(function() {
 			}
 
 		}else{
-			alert("mal");
+			alert("Debe aceptar las condiciones");
 		}
-		
 	});
+	
+
 });
 function validar(){
 	var nombre=$("#name").val();
-		var apellido=$("#lastName").val();
-		var regexNombre = /^[a-zñáéíóúü]+$/gi;
-		var regexApellido = /^[a-zñáéíóúü]+$/gi;
-		if ((regexNombre.test(nombre))&&(regexApellido.test(apellido))) {
-			if((nombre.length>=2 && nombre.length<=20) && (apellido.length>=2 && apellido.length<=20)){
-					return true;
-			}else{
-				alert("cantidad de letras no admitidas");
-			}
+	var apellido=$("#lastName").val();
+	var correo=$("#email").val();
+	var regexNombre = /^[a-zñáéíóúü]+$/gi;
+	var regexApellido = /^[a-zñáéíóúü]+$/gi;
+	var regexCorreo = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+	if ((regexNombre.test(nombre))&&(regexApellido.test(apellido) &&(regexCorreo.test(correo)))) {
+		if((nombre.length>=2 && nombre.length<=20) && (apellido.length>=2 && apellido.length<=20)){
+				return true;
+		}else{
+			alert("cantidad de letras no admitidas");
 		}
-		else{
-			alert("ingrese solo letras");
-		}
+	}
+	else{
+		alert("Ingrese datos correctos");
+	}
+	
 }
 
